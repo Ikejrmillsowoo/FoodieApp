4b4e1d80896d6a721230c05a9cb1f3489674d2b6
poppymills2@gmail.com <poppymills2@gmail.com>
2021-07-28 01:51:36 -0400

    login update

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import { Button } from "reactstrap";
 
 function Login() {
+  const onClick = () => {
+    alert("Login under construction");
+  };
   return (
     <div>
-      <Button color="primary">Log In</Button>
+      <Button onClick={onClick} color="primary">
+        Log In
+      </Button>
     </div>
   );
 }
